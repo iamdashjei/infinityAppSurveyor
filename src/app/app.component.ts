d75c021ce0b9b8902839b57cec675a3e201cba0c
DevTeam <jovitopangan123@gmail.com>
2018-08-27 18:32:01 +0800

    Added to new files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,29 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import firebase from 'firebase';
 import { HomePage } from '../pages/home/home';
+import { DashboardPage } from '../pages/dashboard/dashboard';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = 'LoginPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyC6m-aOof-E-s1AyM0GfgJARfaLHYECRDI",
+      authDomain: "infinityapp-fe5c6.firebaseapp.com",
+      databaseURL: "https://infinityapp-fe5c6.firebaseio.com",
+      projectId: "infinityapp-fe5c6",
+      storageBucket: "infinityapp-fe5c6.appspot.com",
+      messagingSenderId: "918580267251"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +33,3 @@ export class MyApp {
     });
   }
 }
-
---
+++ src/pages/login/login.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
+import firebase from 'firebase';
+
+import { DashboardPage } from '../dashboard/dashboard';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  public recaptchaVerifier:firebase.auth.RecaptchaVerifier; // For captcha verification
+  public email: any;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+  }
+
+  ionViewDidLoad() {
+     this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+  }
+
+  login(phoneNumber: number) {
+    const appVerifier = this.recaptchaVerifier;
+    const phoneNumberString = "+" + phoneNumber;
+    firebase.auth().signInWithPhoneNumber(phoneNumberString, appVerifier)
+      .then( confirmationResult => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        let prompt = this.alertCtrl.create({
+        title: 'Enter the Confirmation code',
+        inputs: [{ name: 'confirmationCode', placeholder: 'Confirmation Code' }],
+        buttons: [
+          { text: 'Cancel',
+            handler: data => { console.log('Cancel clicked'); }
+          },
+          { text: 'Send',
+            handler: data => {
+              confirmationResult.confirm(data.confirmationCode)
+              .then(function (result) {
+                // User signed in successfully.
+                console.log(result.user);
+                alert("Going to dashboardpage");
+              //  this.navCtrl.push(DashboardPage);
+                // ...
+              }).catch(function (error) {
+              //  alert("Confirmation Code is not matched. Please Try Again.");
+              });
+            }
+          }
+        ]
+      });
+      prompt.present();
+    })
+    .catch(function (error) {
+      console.error("SMS not sent", error);
+    });
+
+  }
+
+}
